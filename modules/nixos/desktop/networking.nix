ca7ebffbb5c09efac24a5e099e06b9bcaad195be
Giacomo Di Clerico <giacomodiclerico@gmail.com>
2026-08-11 11:38:49 +0200

    feat: modified networkmanager backend to iwd

--- modules/nixos/desktop/networking.nix
+++ modules/nixos/desktop/networking.nix
@@ -1,5 +1,14 @@
 { ... }:
 
 {
-  networking.networkmanager.enable = true;
+  networking.networkmanager = {
+    enable = true;
+    wifi.backend = "iwd";
+  };
+
+  networking.wireless.iwd.settings = {
+    Settings = {
+      AddressRandomization = "network";
+    };
+  };
 }
