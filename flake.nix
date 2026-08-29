44f503fee6df623b51210c2856f85642d12cbe22
Giacomo Di Clerico <giacomodiclerico@gmail.com>
2026-08-29 10:42:48 +0200

    feat: pinned lanzaboote to master

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
     };
 
     lanzaboote = {
-      url = "github:nix-community/lanzaboote/v1.1.0";
+      url = "github:nix-community/lanzaboote";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
