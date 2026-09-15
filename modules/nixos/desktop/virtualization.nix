54f4a34a23e8e071528b858df9b80328912630bc
Giacomo Di Clerico <giacomodiclerico@gmail.com>
2026-09-15 15:09:21 +0200

    feat: enable virtiofs for shared filesystem

--- modules/nixos/desktop/virtualization.nix
+++ modules/nixos/desktop/virtualization.nix
@@ -1,8 +1,10 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
-  virtualisation.libvirtd.enable = true;
-
+  virtualisation.libvirtd = {
+    enable = true;
+    qemu.vhostUserPackages = with pkgs; [ virtiofsd ];
+  };
   # if you use libvirtd on a desktop environment
   programs.virt-manager.enable = true; # can be used to manage non-local hosts as we
 }
