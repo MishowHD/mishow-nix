74bcc19a9079e709c77bbb72eaf267dda50d4cf0
Giacomo Di Clerico <giacomodiclerico@gmail.com>
2026-09-23 14:40:37 +0200

    feat: added git credential helper

--- modules/home-manager/git.nix
+++ modules/home-manager/git.nix
@@ -1,9 +1,11 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   programs.git = {
     enable = true;
+    package = pkgs.git.override { withLibsecret = true; };
     settings = {
+      credential.helper = "libsecret";
       user = {
         name = "Giacomo Di Clerico";
         email = "giacomodiclerico@gmail.com";
